54b94a6e241ccf21dd416af2e2610c5c05883e31
Alekseev Kirill <mosxee@gmail.com>
2025-08-27 20:09:58 +0300

    test

--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -3,7 +3,7 @@ import { useTranslation } from 'react-i18next';
 const AboutPage = () => {
   const { t } = useTranslation('about');
 
-  return <h2>{t('О сайте')} !!</h2>;
+  return <h2>{t('О сайте')} !!!</h2>;
 };
 
 export default AboutPage;
