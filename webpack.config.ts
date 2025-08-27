5fe1e3acef4da43136e4d0d18c2195da241cc1ea
Alekseev Kirill <mosxee@gmail.com>
2025-08-27 21:07:31 +0300

    test

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,7 +1,7 @@
 import path from 'path';
 import webpack from 'webpack';
-import {buildWebpackConfig} from './config/build/buildWebpackConfig';
-import {BuildPaths, BuildEnv} from './config/build/types/config';
+import { buildWebpackConfig } from './config/build/buildWebpackConfig';
+import { BuildPaths, BuildEnv } from './config/build/types/config';
 
 export default (env: BuildEnv) => {
   const paths: BuildPaths = {
@@ -15,7 +15,7 @@ export default (env: BuildEnv) => {
   const PORT = env.port || 3000;
   const isDev = mode === 'development';
 
-  const config:webpack.Configuration = buildWebpackConfig({
+  const config: webpack.Configuration = buildWebpackConfig({
     mode,
     paths,
     isDev,
