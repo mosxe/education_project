edeca78a7ce27a3982e2078e8a4c7545833c451f
Alekseev Kirill <mosxee@gmail.com>
2025-09-10 01:51:19 +0300

    Добавлена страница Profile

--- src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.test.ts
+++ src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.test.ts
@@ -1,7 +1,5 @@
 import axios from 'axios';
 import { loginByUsername } from './loginByUsername';
-import { StateSchema } from 'app/providers/StoreProvider';
-import { Dispatch } from '@reduxjs/toolkit';
 import { userActions } from 'entities/User';
 import { TestAsyncThunk } from 'shared/lib/tests/TestAsyncThunk/TestAsyncThunk';
 
