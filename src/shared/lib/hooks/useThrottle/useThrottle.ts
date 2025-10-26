3b01b5bca6b3bb6c794cdbf7a5f8498de82f1507
Alekseev Kirill <mosxee@gmail.com>
2025-10-26 18:45:57 +0300

    Добавлено очищение clearTimeout в хуке useThrottle

--- src/shared/lib/hooks/useThrottle/useThrottle.ts
+++ src/shared/lib/hooks/useThrottle/useThrottle.ts
@@ -2,15 +2,20 @@ import { useCallback, useRef } from 'react';
 
 export const useThrottle = (callback: (...arg: any[]) => void, delay: number) => {
   const throttleRef = useRef(false);
+  const timeoutRef = useRef<any>(null);
 
   return useCallback((...arg: any[]) => {
     if (!throttleRef.current) {
       callback(...arg);
       throttleRef.current = true;
 
-      setTimeout(() => {
+      timeoutRef.current = setTimeout(() => {
         throttleRef.current = false;
       }, delay);
     }
+
+    return () => {
+      clearTimeout(timeoutRef.current);
+    };
   }, [callback, delay]);
 };
