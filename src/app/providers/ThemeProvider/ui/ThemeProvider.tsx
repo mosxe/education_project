dcfc7a0b90f46120fac2c720b4e78a13ced46db4
Alekseev Kirill <mosxee@gmail.com>
2025-11-20 11:19:59 +0300

    update React to v18

--- src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -1,13 +1,14 @@
-import { useState, useMemo, FC } from 'react';
+import { useState, useMemo, ReactNode } from 'react';
 import { ThemeContext, LOCAL_STORAGE_THEME_KEY, Theme } from '../lib/ThemeContext';
 
 interface ThemeProviderProps {
   initialTheme?: Theme;
+  children: ReactNode;
 }
 
 const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || Theme.LIGTH;
 
-const ThemeProvider: FC<ThemeProviderProps> = (props) => {
+const ThemeProvider = (props: ThemeProviderProps) => {
   const { initialTheme, children } = props;
   const [theme, setTheme] = useState<Theme>(initialTheme || defaultTheme);
 
